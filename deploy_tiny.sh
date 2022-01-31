b32c7689ca6ee559e1960b6bf3c4f0f13dac7a94
gokulpalani <gokul@montycloud.com>
2022-01-31 20:14:57 +0530

    adding ssm parameter

--- deploy_tiny.sh
+++ deploy_tiny.sh
@@ -4,5 +4,6 @@ if [[ ! -e /opt/tinylogs.txt ]]; then
     touch /opt/tinylogs.txt
 fi
 cd /tinythings/TinyNode
+aws ssm get-parameter  --name tinythings --with-decryption --region ap-northeast-1 --query Parameter.Value --output text >> /tinythings/TinyNode/.env
 npm install
 npm start >> /opt/tinylogs.txt 2>&1 &
